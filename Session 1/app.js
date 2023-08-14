059d942df759aa9a1ced684b7d545f7b231b846c
Abhinav Chandurkar <abhinavchandurkar55@gmail.com>
2023-08-14 20:05:42 +0530

    Session1-part2

--- Session 1/app.js
+++ Session 1/app.js
@@ -1,8 +1,33 @@
-const heading = React.createElement("h1",{id : "heading"},"hello from react");
+/**
+ * Nested 
+ */
 
-console.log(heading);
+// const heading = React.createElement(
+//     "h1",
+//     {id : "parent"},
+//     React.createElement(
+//         "h1",
+//         {id : "child"},
+//         React.createElement(
+//             "h2",
+//             {},
+//             "I am h2 tag"
+//         )
+//     )
+// );
 
-const root =  ReactDOM.createRoot(document.getElementById("root"));
+//** using array of children */
+
+const content = React.createElement(
+    "div",
+    {},
+    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+)
 
 
-root.render(heading);
+console.log(content);//returns an object
+
+const root =  ReactDOM.createRoot(document.getElementById("root"));//converts the js obj/react element to html tag 
+
+
+root.render(content);
